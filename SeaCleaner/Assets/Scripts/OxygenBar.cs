f460d5d7ad54e324ae7e08aee9e7a8ee5ed39f65
triscakepuff <tristanchandra90@gmail.com>
2024-07-29 22:45:47 +0700

    Oxygen bar UI
    oxygen bar UI, and trash animation

--- SeaCleaner/Assets/Scripts/OxygenBar.cs
+++ SeaCleaner/Assets/Scripts/OxygenBar.cs
@@ -9,30 +9,39 @@ public class OxygenBar : MonoBehaviour
     public float maxOxygen = 100f;
     public float currentOxygen;
     public float oxygenReduceRate = 1f;
+    private float oxygenPercentage;
 
     [Header("Call RestoreOxygen() to restore by the amount below")]
     [Header("you can also call RestoreOxygen(*amount of oxygen restored*)")]
     public float oxygenRestoration = 100f;
 
-    public Image bar;
+    [Header("UI")]
+    public Slider slider;
+    public Image statusImage;
+    public Sprite Full;
+    public Sprite halfFull;
+    public Sprite quarterFull;
+    public Sprite none;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bar = GetComponent<Image>();
-        bar.fillMethod = Image.FillMethod.Horizontal;
+        SetMaxOxygen();
         currentOxygen = maxOxygen;
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetOxygen();
+        CheckOxygenPercentage();
+        Debug.Log(oxygenPercentage);
         if(currentOxygen > maxOxygen)
         {
             currentOxygen = maxOxygen;
         }
 
         currentOxygen = currentOxygen - (oxygenReduceRate * Time.deltaTime);
-        bar.fillAmount = currentOxygen / maxOxygen;
 
         if(currentOxygen <= 0)
         {
@@ -48,5 +57,33 @@ public class OxygenBar : MonoBehaviour
     public void RestoreOxygen(float oxygenRestorationCustom)
     {
         currentOxygen = currentOxygen + oxygenRestorationCustom;
+    }
+
+    public void SetOxygen()
+    {
+        slider.value = currentOxygen;
+    }
+
+     public void SetMaxOxygen()
+    {
+        slider.maxValue = maxOxygen;
+        slider.value = currentOxygen;
+    }
+    public void CheckOxygenPercentage()
+    {
+        oxygenPercentage = (currentOxygen / maxOxygen) * 100;
+        if(oxygenPercentage <= 100f && oxygenPercentage > 50f)
+        {
+            statusImage.sprite = Full;
+        }else if((oxygenPercentage <= 50f && oxygenPercentage > 25f))
+        {
+            statusImage.sprite = halfFull;
+        }else if((oxygenPercentage <= 25f && oxygenPercentage > 0f))
+        {
+            statusImage.sprite = quarterFull;
+        }else if(oxygenPercentage <= 0f)
+        {
+            statusImage.sprite = none;
+        }
     }
 }
